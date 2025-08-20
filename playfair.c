c3414de12c1c966de48b09c61fb858d4a93d420c
Giovanna <gidallefiori@outlook.com>
2025-08-20 13:02:54 -0300

    fazer liberação de memória

--- playfair.c
+++ playfair.c
@@ -13,7 +13,7 @@
 #include<stdlib.h>
 
 struct texto_t{
-    unsigned int tamanho;           //número de caracteres do texto sem espaços
+    unsigned int tamanho;           //número de caracteres do texto com espaços
     unsigned int num_pares;         //quantidade de parzinhos formados
     char *texto_base;               //texto base com espaços e letras de separação/complemento (x)
 };
@@ -55,16 +55,28 @@ void trata_texto(char *arquivo, struct texto_t *texto)
     
     rewind(arquivo_original);
 
-    //Remove os espaços
+    //Conta o número de caracteres
     int i = 0;
-    while (letra = fgetc(arquivo_original) != EOF)
+    while ((letra = fgetc(arquivo_original)) != EOF)
     {
-        if (letra != " ")
-        {
-            texto->texto_base = letra;
-            texto->tamanho++;
-        }
+        texto->tamanho++;
+        i++;
     }
+
+    //Aloca memória para texto_base com base no número de carcteres
+    texto->texto_base = malloc(texto->tamanho * sizeof(char));
+
+    //Remove os espaços
+    i = 0;
+    while ((letra = fgetc(arquivo_original)) != EOF)
+    {
+        if (letra != ' ')                           //se não for um espaço
+            texto->texto_base[i] = letra;           //guarda a letra
+        
+        i++;                                        //avança para o próximo campo do vetor
+    }
+
+    printf("Tamanho do texto sem espaços: %d\n", texto->tamanho);
 }
 
 void forma_pares(struct texto_t texto)
@@ -91,18 +103,52 @@ void decifra(struct playfair_t playfair)
 
 int main()
 {
+    char *chave;
     char *arquivo;
     struct texto_t *texto;
+    struct playfair_t *playfair;
 
-    //Inicializações
-    texto = malloc(sizeof(texto));
+    //Inicializações -----------------------------------------------------
+    texto = malloc(sizeof(struct texto_t));
+    playfair = malloc(sizeof(struct playfair_t));
+    if (!texto)
+    {
+        printf("Erro ao alocar memória para texto.\n");
+        return(-1);
+    }
+    if(!playfair)
+    {
+        printf("Erro ao alocar memória para playfair.\n");
+        return(-1);
+    }
 
-    printf("Este código cifra um texto utilizando a Cifra Playfair.\n\n");
-    printf("Arquivo a ser cifrado: %s\n", arquivo);
+    arquivo = malloc(256 * sizeof(char));
+    if (!arquivo)
+    {
+        printf("Erro ao alocar memória para o caminho do arquivo.\n");
+        return(-1);
+    }
+    chave = malloc(256 * sizeof(char));
+    if (!chave)
+    {
+        printf("Erro ao alocar memória para a chave.\n");
+        return(-1);
+    }
+
+    inicializa_texto(texto);
+    inicializa_playfair(playfair);
+    //--------------------------------------------------------------------
+
+    printf("Este código cifra um texto utilizando a Cifra Playfair.\n");
+    printf("Caminho do arquivo a ser cifrado: ");
+    scanf("%255s", arquivo);
+    printf("Digite sua chave: ");
+    scanf("%255s", chave);
 
     //Trata o texto
     trata_texto(arquivo, texto);
 
     //Desacolações
     free(texto);
+    free(playfair);
 }
